67d28f9f028448e317fcf2eee7356100a7905493
Dany Lasso <dannylasso0990@hotmail.com>
2017-10-25 11:55:24 -0500

    implementacion Chicken

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,12 @@
 public class HelloWord{
-	public satatic void main(String[] args){
-		System.out.println("HOLA MUNDO");
+	public static void main(String[] args){
+		System.out.println("Hello Danny");
+		Chicken chicken;
+		//inicializar el objeto
+		chicken = new Chicken();
+		chicken.initializaData("Lucy","cafe",2);
+		chicken.print();
+		//s.o.p´(chicken.name)-->no funciona la variable es privada
 	}
+	
 }
