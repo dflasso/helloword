039ab9810d6735eaf787c7c10757e3e5ae07c90f
dflasso <dannylasso0909@hotmail.com>
2017-10-26 21:40:22 -0500

    Creacion metodos : affirm and negate

--- Chicken.java
+++ Chicken.java
@@ -5,20 +5,29 @@ public class Chicken{
 		private int age;
 		//metodos
 		public void layAnEgg(){
-			System.out.println("The chicken lay an egg");
+			System.out.println("lay an egg");
 		}
 		public void poop(){
-			System.out.println("The Chicken poop");
+			System.out.println("poop");
 		}
 		public void drink(){
-			System.out.println("The chicken drink");
+			System.out.println("is drinking");
 		}
 		public void print(){
-			System.out.println("The name of the chicken" +name+ ", color  " +color+ ", age  " +age);
+			System.out.println("The name of the chicken  " +name+ ", color  " +color+ ", age  " +age);
 		}
 		public void initializaData(String name,String color,int age){
 			this.name=name;
 			this.color=color;
 			this.age=age;
+		}
+		public void negate(){
+			System.out.println("The Chicken don't");
+		}
+		public void affirm(){
+			System.out.println("The Chicken ");
+		}
+		public void separate(){
+			System.out.println("-----------------------------------------");
 		}
 }
