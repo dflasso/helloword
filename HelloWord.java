3c663564b929cef0d0e6d3d24483d294e54ea90c
dflasso <dannylasso0909@hotmail.com>
2017-10-26 21:45:32 -0500

    implementacion de 4 gallinas

--- HelloWord.java
+++ HelloWord.java
@@ -1,12 +1,62 @@
 public class HelloWord{
 	public static void main(String[] args){
 		System.out.println("Hello Danny");
-		Chicken chicken;
-		//inicializar el objeto
-		chicken = new Chicken();
-		chicken.initializaData("Lucy","cafe",2);
-		chicken.print();
+		//declaracion del primer objeto
+		Chicken chickenFirst;
+		//inicializar el primer objeto
+		chickenFirst = new Chicken();
+		chickenFirst.initializaData("Lucy","cafe",2);
+		chickenFirst.print();
 		//s.o.p´(chicken.name)-->no funciona la variable es privada
+		chickenFirst.affirm();
+		chickenFirst.layAnEgg();
+		chickenFirst.affirm();
+		chickenFirst.poop();
+		chickenFirst.affirm();
+		chickenFirst.drink();
+		//
+		//declaracion del segundo objeto
+		Chicken chickenSecond;
+		//inicializar el segundo objeto
+		chickenSecond = new Chicken();
+		chickenSecond.separate();
+		chickenSecond.initializaData("Pepita","negra",1);
+		chickenSecond.print();
+		chickenSecond.affirm();
+		chickenSecond.layAnEgg();
+		chickenSecond.affirm();
+		chickenSecond.poop();
+		chickenSecond.negate();
+		chickenSecond.drink();
+		//
+		//declaracion del tercero  objeto
+		Chicken chickenThird;
+		//inicializar el tercer objeto
+		chickenThird = new Chicken();
+		//llamada a los metodos
+		chickenThird.separate();
+		chickenThird.initializaData("Turuleca","blanca",3);
+		chickenThird.print();
+		chickenThird.negate();
+		chickenThird.layAnEgg();
+		chickenThird.affirm();
+		chickenThird.poop();
+		chickenThird.affirm();
+		chickenThird.drink();
+		//
+		//declaracion del cuarto objeto
+		Chicken chickenFourth;
+		//inicializar el cuarto objeto
+		chickenFourth = new Chicken();
+		chickenFourth.separate();
+		chickenFourth.initializaData("de los huevos de oro","amarilla",2);
+		chickenFourth.print();
+		chickenFourth.affirm();
+		chickenFourth.layAnEgg();
+		chickenFourth.negate();
+		chickenFourth.poop();
+		chickenFourth.affirm();
+		chickenFourth.drink();
 	}
 	
 }
